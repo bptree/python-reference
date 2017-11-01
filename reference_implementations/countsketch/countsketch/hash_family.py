9caebd599ba12f6b9b5d0648b9e5ff7d3abddc14
Bailey Parker <b@ileyparker.com>
2017-11-01 14:04:32 -0400

    Make strongly universal_k hash family more robust

--- reference_implementations/countsketch/countsketch/hash_family.py
+++ reference_implementations/countsketch/countsketch/hash_family.py
@@ -1,5 +1,5 @@
 from random import randrange
-from typing import Any, Generic, Hashable, Sequence, Tuple, TypeVar
+from typing import Any, Generic, Hashable, Sequence, TypeVar
 
 V = TypeVar('V')
 H = TypeVar('H', bound=Hashable)
@@ -8,23 +8,35 @@ LARGE_PRIME = 2**61 - 1
 
 
 class Hash(Generic[V]):
-    def __init__(self, params: Tuple[int], image: Sequence[V]) -> None:
-        self._params = params
+    def __init__(self, coefficients: Sequence[int], image: Sequence[V],
+                 prime: int = LARGE_PRIME) -> None:
+        # TODO: assert prime is actually prime
+        if len(image) > prime:
+            raise ValueError('prime field chosen not large enough for image')
+
+        self._coefficients = tuple(coefficients)
+        self._prime = prime
         self._image = image
 
     def __call__(self, item: H) -> V:
         x = hash(item)
-        y = sum(c * x**p for p, c in enumerate(self._params))
+        y = sum(c * x**p
+                for p, c in enumerate(self._coefficients))  # type: int
 
-        return self._image[(y % LARGE_PRIME) % len(self._image)]
+        return self._image[(y % self._prime) % len(self._image)]
 
     def __eq__(self, other: Any) -> bool:
-        return isinstance(other, Hash) and self._params == other._params
+        return isinstance(other, Hash) and \
+                self._coefficients == other._coefficients
 
 
-def generate_hash(k_independence: int, image: Sequence[V]) -> Hash[V]:
-    return Hash([_random_coefficient(i == 0) for i in range(k_independence)],
-                image)
+def generate_hash(k_independence: int, image: Sequence[V],
+                  prime: int = LARGE_PRIME) -> Hash[V]:
+    if k_independence < 1:
+        raise ValueError('k_independence must be greater than 0')
+
+    coefficients = [_random_coefficient(i == 0) for i in range(k_independence)]
+    return Hash(coefficients, image, prime)
 
 
 def _random_coefficient(allow_zero: bool = False) -> int:
