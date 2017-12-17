0ce66a62d6ca6f52c8a6b9beb2122ee20c0a2ccd
Samuel Beckley <sobeckley@gmail.com>
2017-12-17 18:46:29 -0500

    Brute Force f2_estimator done, not tested - Rachel

--- reference_implementations/bptree/f2_estimator.py
+++ reference_implementations/bptree/f2_estimator.py
@@ -3,12 +3,22 @@ class F2_Estimator(object):
     def __init__(self):
         pass
 
-    def update_estimate(self, item):
+    def add_item(self, item):
         raise NotImplementedError
 
-    def get_value(self):
+    def update_estimate(self):
         raise NotImplementedError
 
 class Brute_Force_F2_Estimator(F2_Estimator):
     def __init__(self, n):
         self.f = [0]*n
+        self.F2 = 0
+
+    def add_item(self, item):
+        self.f[item] += 1
+
+    def update_estimate(self):
+        self.F2 = 0
+        for freq in self.f:
+            self.F2 += freq**2
+        return self.F2
